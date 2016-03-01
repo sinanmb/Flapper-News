1ff09e22f94d491b728603632b87380f4336558a
Sinan <sinan@mouline.net>
2016-03-01 12:20:48 -0800

    All the way until The Posts Page

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 /**
  * Created by Sinan on 2/28/16.
  */
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }
+]);
+
+app.factory('posts', [function () {
+    var o = {
+        posts: [{title:'Hello', link:'', upvotes:0}]
+    };
+
+    return o;
+}]);
 
 app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
+    '$scope', 'posts',
+    function($scope, posts){
 
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
+        $scope.posts = posts.posts;
 
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') { return; }
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes:0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong post!', upvotes:0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
@@ -30,3 +57,17 @@ app.controller('MainCtrl', [
             post.upvotes += 1;
         };
     }]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+        if(!$scope.body || $scope.body === '') { return; }
+        $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+        });
+        $scope.body = '';
+    };
+}]);
